f10b1f5037943875793f395696499105d3439408
anushree <anushreev53@gmail.com>
2025-01-07 10:38:06 +0530

    update

--- contact.php
+++ contact.php
@@ -1,19 +1,24 @@
-  <?php
-	if (isset($_POST["submit"])) {
-		$name = $_POST['name'];
-		$email = $_POST['email'];
-		$message = $_POST['message'];
-		$from = 'Parax Contact Form'; 
-		$to = 'example@gmail.com'; 
-		$subject = $_POST['subject']; 
-		
-		$body = "From: $name\n E-Mail: $email\n Subject: $subject\n Message:\n $message";
-	
+<?php
+if (isset($_POST["submit"])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $subject = $_POST['subject'];
 
-	mail($to, $subject, $body, $from) or die("Error!");
+    $to = 'anushreev813@gmail.com'; // Your email address
+    $from = 'From: Parax Contact Form <no-reply@yourdomain.com>'; // Adjust the sender email as needed
+    $body = "You have received a new message from your website contact form.\n\n" .
+            "Here are the details:\n" .
+            "Name: $name\n" .
+            "Email: $email\n" .
+            "Subject: $subject\n" .
+            "Message:\n$message\n\n" .
+            "This message was sent on " . date('Y-m-d H:i:s');
 
-	header("location: thank-you.html");
-	
-	}
-	
+    if (mail($to, $subject, $body, $from)) {
+        header("Location: thank-you.html");
+    } else {
+        echo "Error sending email!";
+    }
+}
 ?>
